1ed7485fb0c8fef6c848c89a8f54066fee115797
Brian Lau <brian.lau@upmc.fr>
2014-12-29 16:49:34 +0100

    Avoid fieldtrip error, need to verify correctness

--- subtrees/Process/@SampledProcess/convert2Fieldtrip.m
+++ subtrees/Process/@SampledProcess/convert2Fieldtrip.m
@@ -5,6 +5,6 @@ for i = 1:size(win,1)
    dat.trial{i} = self.values{i}';
    dat.time{i} = self.times{i}' - self.times{i}(1);
 end
-dat.sampleinfo = self.window*self.Fs;
+dat.sampleinfo = floor(self.window*self.Fs);
 dat.sampleinfo = dat.sampleinfo + 1;
 dat.label = self.labels;
