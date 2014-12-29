eb85126945d01fa35192a55f921d97c81e395dfd
Brian Lau <brian.lau@upmc.fr>
2014-12-29 16:33:12 +0100

    Separate preprocessing, working for continuous (baseline) data

---
+++ Analyses/preprocess.m
@@ -0,0 +1,77 @@
+% Preprocess raw LFP data
+%
+function preprocess(varargin)
+
+p = inputParser;
+p.KeepUnmatched = true;
+addParamValue(p,'basedir','/Volumes/Data/Human/',@ischar);
+addParamValue(p,'savedir',pwd,@ischar);
+addParamValue(p,'area','STN',@ischar);
+addParamValue(p,'patient','',@ischar);
+addParamValue(p,'recording','Postop',@ischar);
+addParamValue(p,'protocol','',@ischar);
+addParamValue(p,'task','',@ischar);
+addParamValue(p,'condition','',@ischar);
+addParamValue(p,'run','',@isscalar);
+
+% Preprocessing
+addParamValue(p,'trim',1.5,@isscalar);
+addParamValue(p,'deline',false,@islogical);
+
+% additional info to store in SampledProcess
+addParamValue(p,'data',[]);
+addParamValue(p,'dataName',@ischar);
+
+% Saving
+addParamValue(p,'overwrite',false,@islogical);
+
+parse(p,varargin{:});
+p = p.Results;
+
+info = filterFilename(fullfile(p.basedir,p.area,p.patient,p.recording));
+info = filterFilename(info,'protocol',p.protocol,'task',...
+   p.task,'condition',p.condition,'run',p.run);
+
+if isempty(info)
+   return;
+end
+
+files = buildFilename(info);
+
+if ~isempty(files)
+   % Savename
+   ind = findstr(files{1},'_RUN');
+   [~,fname] = fileparts(files{1}(1:ind-1));
+   fname = [fname '_PRE'];
+   
+   if exist(fullfile(p.savedir,[fname '.mat']),'file') && ~p.overwrite
+      fprintf('File found, skipping\n');
+      return;
+   end
+   
+   switch lower(p.task)
+      case {'msup'} 
+            
+      otherwise
+      % load each run, keep as separate object
+      for i = 1:numel(files)
+         s(i) = loadSingleRun(files{i},'trim',p.trim,'deline',p.deline);
+
+         % TODO: allow for each run to have different data
+         if not(isempty(p.data))
+            if isempty(p.dataName)
+               s(i).info('data') = p.data;
+            else
+               s(i).info(p.dataName) = p.data;
+            end
+         end
+      end
+   end
+      
+   % Save
+   save(fullfile(p.savedir,fname),'s');
+   
+   clear s;
+else
+   warning('requested, but not found');
+end
