ea64041b93df75aa6ffd0fe7005c98bb5335fd9c
Brian Lau <brian.lau@upmc.fr>
2014-12-30 19:59:13 +0100

    ENH: substitute strsplit for Matlab < 2012a ENH: Handle filename without condition

--- Data/parseFilename.m
+++ Data/parseFilename.m
@@ -9,19 +9,20 @@ end
 
 [path,name,ext] = fileparts(f);
 
-C = strsplit(name,'_');
+%C = strsplit(name,'_');
+C = regexp(name,'_','split');
 
-if numel(C) ~= 7
-   info.path = '';
-   info.patientID = '';
-   info.date = '';
-   info.data = '';
-   info.protocol = '';
-   info.task = '';
+if numel(C) == 6
+   info.path = path;
+   info.patientID = C{1};
+   info.date = C{2};
+   info.data = C{3};
+   info.protocol = C{4};
+   info.task = C{5};
    info.condition = '';
-   info.run = '';
-   info.filetype = '';
-else
+   info.run = C{6};
+   info.filetype = ext;
+elseif numel(C) == 7
    info.path = path;
    info.patientID = C{1};
    info.date = C{2};
@@ -31,4 +32,14 @@ else
    info.condition = C{6};
    info.run = C{7};
    info.filetype = ext;
+else
+   info.path = '';
+   info.patientID = '';
+   info.date = '';
+   info.data = '';
+   info.protocol = '';
+   info.task = '';
+   info.condition = '';
+   info.run = '';
+   info.filetype = '';
 end
