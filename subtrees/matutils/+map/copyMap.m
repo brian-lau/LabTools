625f93f2cf2b4af795e222724581c8ae46fe7a61
Brian Lau <brian.lau@upmc.fr>
2015-01-04 13:18:30 +0100

    ENH: default to copying entire dictionary

--- subtrees/matutils/+map/copyMap.m
+++ subtrees/matutils/+map/copyMap.m
@@ -2,12 +2,19 @@
 % from another
 % copyKeys must be cell array of valid keys, 
 % newKeys must have the same size as copyKeys
-function newMap = copymap(map,copyKeys,newKeys)
+function newMap = copyMap(m,copyKeys,newKeys)
+
+if nargin < 2
+   copyKeys = m.keys;
+end
 
 if nargin < 3
    newKeys = copyKeys;
+else
+   if numel(copyKeys) ~= numel(newKeys)
+      error('# of newKeys must match # requested in copyKeys');
+   end
 end
 
-vals = map.values(copyKeys);
-
+vals = m.values(copyKeys);
 newMap = containers.Map(newKeys,vals);
