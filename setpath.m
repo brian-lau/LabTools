79d82fca67daa102d8f0d61ab85c7c665658861f
Brian Lau <brian.lau@upmc.fr>
2015-01-10 20:09:27 +0100

    Reminder to remove fieldtrip path clashes

--- setpath.m
+++ setpath.m
@@ -8,3 +8,5 @@ end
 addpath(fullfile(basedir,'subtrees','matutils'));
 pathStr = os.genpath_exclude(basedir,{'.git' 'Testing'});
 addpath(pathStr);
+
+% check for and remove certain fieldtrip paths which shadow functions
