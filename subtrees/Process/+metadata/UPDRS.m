a37a9dc279f75e0b329c1bb47657a06a30b57f3a
Brian Lau <brian.lau@upmc.fr>
2015-01-12 18:44:22 +0100

    Work on UPDRS class

--- subtrees/Process/+metadata/UPDRS.m
+++ subtrees/Process/+metadata/UPDRS.m
@@ -1,7 +1,7 @@
 classdef UPDRS < metadata.Exam
    properties
       
-      LEDD %?
+      %LEDD %?
       item1
       item2
       item3
@@ -19,9 +19,47 @@ classdef UPDRS < metadata.Exam
       item15
       item16
       item17
+      item18
+      item19
+      item20a
+      item20b
+      item20c
+      item20d
+      item20e
+      item21a
+      item21b
+      item22a
+      item22b
+      item22c
+      item22d
+      item22e
+      item23a
+      item23b
+      item24a
+      item24b
+      item25a
+      item25b
+      item26a
+      item26b
+      item27
+      item28
+      item29
+      item30
+      item31
+      item32
+      item33
+      item34
+      item35
+      item36
+      item37
+      item38
+      item39
+      item40
+      item41
+      item42
       
       HoehnYahr
-      
+      SchwabEngland
    end
    properties(SetAccess = private, Dependent = true, Transient = true)
       akinesia
