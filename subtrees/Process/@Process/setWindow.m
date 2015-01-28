885d95209f04c8eeaaaf68919fa8ea06fcc86de3
Brian Lau <brian.lau@upmc.fr>
2015-01-28 17:42:08 +0100

    ENH: Allow single window as cell array

--- subtrees/Process/@Process/setWindow.m
+++ subtrees/Process/@Process/setWindow.m
@@ -14,8 +14,12 @@ n = numel(self);
 if n == 1
    % single or multiple windows
    if ~isnumeric(window)
-      error('Process:setWindow:InputFormat',...
-         'Window for a scalar process must be a numeric [nWin x 2] array.');
+      if (numel(window)==1) && all([1 2]==size(window{1}))
+         window = window{1};
+      else
+         error('Process:setWindow:InputFormat',...
+            'Window for a scalar process must be a numeric [nWin x 2] array.');
+      end
    end
    self.window = self.checkWindow(window,size(window,1));
 else
