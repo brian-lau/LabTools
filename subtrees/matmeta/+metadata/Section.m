858a9febc53f6303228f75632df01776d3ad0b1f
Brian Lau <brian.lau@upmc.fr>
2015-02-05 11:53:38 +0100

    ENH: subclass mixin.Copyable

--- subtrees/matmeta/+metadata/Section.m
+++ subtrees/matmeta/+metadata/Section.m
@@ -1,5 +1,5 @@
 % Section
-classdef Section < hgsetget & matlab.mixin.Heterogeneous
+classdef Section < hgsetget & matlab.mixin.Heterogeneous & matlab.mixin.Copyable
    properties
       type
    end
