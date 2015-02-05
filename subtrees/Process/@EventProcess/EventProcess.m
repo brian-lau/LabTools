c1799a5e3f5b229e6ed573fb1cd2a4c28db08a95
Brian Lau <brian.lau@upmc.fr>
2015-02-05 14:07:01 +0100

    BF: error when property is missing

--- subtrees/Process/@EventProcess/EventProcess.m
+++ subtrees/Process/@EventProcess/EventProcess.m
@@ -71,7 +71,8 @@ classdef(CaseInsensitiveProperties, TruncatedProperties) EventProcess < PointPro
                   % This must pr
                   query.where(args.(fn{i}));
                elseif ischar(args.(fn{i}))
-                  query.where(@(x) strcmp(x.(fn{i}),args.(fn{i})));
+                  query.where(@(x) isprop(x,fn{i}))...
+                       .where(@(x) strcmp(x.(fn{i}),args.(fn{i})));
                else
                   % attempt equality
                   query.where(@(x) x.(fn{i})==args.(fn{i}));
