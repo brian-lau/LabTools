aec940e0abe5a9bdfd2a3a4c10a34341f8fcdd46
Brian Lau <brian.lau@upmc.fr>
2015-02-09 08:10:52 +0100

    initial commit

---
+++ subtrees/matutils/nearest.m
@@ -0,0 +1,15 @@
+%http://stackoverflow.com/questions/22609192/find-the-closest-value-in-a-matrix-matlab
+function [val,row,col] = nearest(ref,matrix)
+
+% linear index of closest entry
+if isscalar(ref)
+   [~,ii] = min(abs(matrix(:)-ref));
+else
+   [~,ii] = min(abs(bsxfun(@minus,matrix(:),ref)));
+end
+val = matrix(ii);
+
+if nargout > 1
+   %% Convert the linear index into row and column numbers
+   [row,col] = ind2sub(size(matrix),ii);
+end
