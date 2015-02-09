a8665607b28cb3d736caf28df5e26c9ae64e9a6a
Brian Lau <brian.lau@upmc.fr>
2015-02-09 18:15:06 +0100

    add PointProcess to Segment example

--- subtrees/Process/@EventProcess/applyOffset.m
+++ subtrees/Process/@EventProcess/applyOffset.m
@@ -18,8 +18,12 @@ for i = 1:numel(offset)
       self.times{i,j} = self.times{i,j} + offset(i);
 
       temp = self.values{i,j};
-      
       [temp.tStart] = list([temp.tStart] + offset(i));
       [temp.tEnd] = list([temp.tEnd] + offset(i));
+      
+%       for k = 1:numel(self.values{i,j})
+%          self.values{i,j}(k).tStart = self.values{i,j}(k).tStart + offset(i);
+%          self.values{i,j}(k).tEnd = self.values{i,j}(k).tEnd + offset(i);
+%       end
    end
 end
