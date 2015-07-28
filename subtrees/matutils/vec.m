8b8fa9e04416db25e7de80fb0979bde37013d6b3
Brian Lau <brian.lau@upmc.fr>
2015-07-28 14:59:20 +0200

    initial commit - row

--- subtrees/matutils/vec.m
+++ subtrees/matutils/vec.m
@@ -1,4 +1,4 @@
-% Create a column vector from a matrix X = [x1 x2 ? ? ? xn] by stacking columns
+% Create a column vector from a matrix X = [x1 x2 ... xn] by stacking columns
 
 function y = vec(X)
 
