a624e9464933dc072ccf7e2cc2eb3a83f585a48d
Brian Lau <brian.lau@upmc.fr>
2015-08-03 16:15:15 +0200

    Move find() to separate file

--- subtrees/Process/@EventProcess/EventProcess.m
+++ subtrees/Process/@EventProcess/EventProcess.m
@@ -57,40 +57,7 @@ classdef(CaseInsensitiveProperties) EventProcess < PointProcess
          end
       end
       
-      function events = find(self,varargin)
-         p = inputParser;
-         p.KeepUnmatched= true;
-         p.FunctionName = 'EventProcess find';
-         p.parse(varargin{:});
-         args = p.Unmatched;
-
-         query = linq(self.values{1});
-         fn = fieldnames(args);
-         for i = 1:numel(fn)
-            if query.count>0
-               if isa(args.(fn{i}),'function_handle')
-                  % This must evaluate to a boolean
-                  query.where(args.(fn{i}));
-               elseif ischar(args.(fn{i}))
-                  try
-                     query.where(@(x) strcmp(x.(fn{i}),args.(fn{i})));
-                  catch
-                  end
-%                   query.where(@(x) isprop(x,fn{i}))...
-%                        .where(@(x) strcmp(x.(fn{i}),args.(fn{i})));
-               else
-                  % attempt equality
-                  query.where(@(x) x.(fn{i})==args.(fn{i}));
-               end
-            end
-         end
-                  
-         if query.count > 0
-            events = query.toArray();
-         else
-            events = self.nullEvent;
-         end
-      end
+      ev = find(self,varargin)
       
       % add event
       % remove event
