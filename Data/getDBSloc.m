b6725d4baa15e20df7eefcf8ae62eda41866dcc9
Brian Lau <brian.lau@upmc.fr>
2015-08-04 11:52:39 +0200

    Update to Sara's new text format

--- Data/getDBSloc.m
+++ Data/getDBSloc.m
@@ -1,24 +1,27 @@
 % patient - 4 letter id string
 % coord - 'acpc' or 'stn'
-% dipole - '01G' '23D', etc
+% dipole - '01' '23', etc
+% side - 'D' or 'G'
 
 % x,y,z are localizations depending on coordinate system
 
-function [x,y,z] = getDBSloc(patient,coord,dipole,side)
+function [x,y,z] = getDBSloc(fname,patient,coord,dipole,side)
 
 persistent text patients sides coords dipoles X Y Z;
 
 if isempty(text)
-   fid = fopen('/Users/brian/Downloads/NormalizedACPC_YEB_Coordinates.txt');
    
-   text = {};
-   while 1
-      tline = fgetl(fid);
-      if ~ischar(tline)
-         fclose(fid);
-         break;
-      else
-         text = cat(1,text,tline);
+   if isempty(text)
+      fid = fopen(fname);
+      text = {};
+      while 1
+         tline = fgetl(fid);
+         if ~ischar(tline)
+            fclose(fid);
+            break;
+         else
+            text = cat(1,text,tline);
+         end
       end
    end
    
@@ -29,26 +32,26 @@ if isempty(text)
    for i = 1:numel(text)
       temp = strsplit(text{i});
       temp2 = strsplit(temp{2},'_');
-      patients{i,1} = temp2{end};
+      patients{i,1} = temp2{end}(1:4);
 
       temp2 = strsplit(temp{3},'_');
-      if strcmp(temp2{1}(1),'L')
+      if strcmp(temp2{6}(1),'L')
          sides{i,1} = 'G';
       else
          sides{i,1} = 'D';
       end
       
-      dipoles{i,1} = [temp2{3}(1) temp2{3}(3)];
+      dipoles{i,1} = [temp2{end}(1) temp2{end}(3)];
       
-      if strcmp(temp2{3}(4),'A')
+      if strcmp(temp{4}(1),'A')
          coords{i,1} = 'ACPC';
       else
          coords{i,1} = 'STN';
       end
       
-      X(i,1) = str2num(temp{5}(2:end));
-      Y(i,1) = str2num(temp{7}(2:end));
-      Z(i,1) = str2num(temp{9}(2:end));
+      X(i,1) = str2num(temp{6}(2:end));
+      Y(i,1) = str2num(temp{8}(2:end));
+      Z(i,1) = str2num(temp{10}(2:end));
    end
 end
 
@@ -57,7 +60,6 @@ if nargin == 3
    dipole = temp(1:2);
    side = temp(3);
 end
-
 
 ind = strncmpi(patient,patients,numel(patient)) & strcmpi(coords,coord) & strcmpi(dipoles,dipole) & strcmpi(sides,side);
 
