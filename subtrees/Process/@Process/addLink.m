e16f46f40fa8ac55a8902327a9773f2307e4c0a4
Brian Lau <brian.lau@upmc.fr>
2015-08-13 11:41:30 +0200

    ENH: use hgsetget format for linking setter calls

--- subtrees/Process/@Process/addLink.m
+++ subtrees/Process/@Process/addLink.m
@@ -5,8 +5,16 @@ function addLink(self,varargin)
 
 % assert numel(stack)>=2?
 stack = dbstack('-completenames');
+f = stack(2).name; % maybe take from end of stack?
+
+% Setter calls using set()format
+if strfind(f,'.set.')
+   temp = regexp(f,'\.','split');
+   varargin = [temp(3) varargin];
+   f = 'set';
+end
 
 n = size(self.chain,1) + 1;
-self.chain{n,1} = stack(2).name; % maybe take from end of stack? 
+self.chain{n,1} = f;
 self.chain{n,2} = varargin;
 self.chain{n,3} = ~self.lazyEval;
