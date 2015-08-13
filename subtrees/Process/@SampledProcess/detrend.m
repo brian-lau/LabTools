171c6597abf26ecfa7173548f87b6b0f83035a56
Brian Lau <brian.lau@upmc.fr>
2015-08-13 11:53:35 +0200

    ENH: chainable

--- subtrees/Process/@SampledProcess/detrend.m
+++ subtrees/Process/@SampledProcess/detrend.m
@@ -3,6 +3,15 @@
 function self = detrend(self)
 
 for i = 1:numel(self)
+   %-- Add link to function chain ----------
+   if ~self(i).running_
+      addLink(self(i));
+      if self(i).lazyEval
+         continue;
+      end
+   end
+   %----------------------------------------
+
    for j = 1:size(self(i).window,1)
       self(i).values{j} = bsxfun(@minus,self(i).values{j},...
          nanmean(self(i).values{j}));
