f909fc9bff43725a79aac7396e168aa16f172860
Brian Lau <brian.lau@upmc.fr>
2015-08-14 11:41:07 +0200

    possible to capture outputs, but must specify number

--- subtrees/Process/@Process/addToQueue.m
+++ subtrees/Process/@Process/addToQueue.m
@@ -3,6 +3,7 @@
 
 % {function_name args status}
 
+%function addToQueue(self,nout,varargin)
 function addToQueue(self,varargin)
 
 if self.running_
@@ -13,7 +14,7 @@ end
 stack = dbstack('-completenames');
 f = stack(2).name; % maybe take from end of stack?
 
-% Setter calls using set()format
+% Setter calls using set()format (hgsetget)
 if strfind(f,'.set.')
    temp = regexp(f,'\.','split');
    varargin = [temp(3) varargin];
@@ -25,4 +26,5 @@ for i = 1:numel(self)
    self(i).queue{n,1} = f;
    self(i).queue{n,2} = varargin;
    self(i).queue{n,3} = ~self(i).lazyEval;
+   %self(i).queue{n,4} = nout;
 end
