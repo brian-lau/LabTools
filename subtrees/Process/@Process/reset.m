f6b09748f6f9a3f13cdbd72493a61fcbe1337fc0
Brian Lau <brian.lau@upmc.fr>
2015-08-14 11:42:04 +0200

    ENH: selectable reset

--- subtrees/Process/@Process/reset.m
+++ subtrees/Process/@Process/reset.m
@@ -4,10 +4,20 @@
 % SEE ALSO
 % setInclusiveWindow
 
-function self = reset(self)
+function self = reset(self,n)
+
+if nargin < 2
+   n = 0;
+else
+   n = max(n,0);
+end
 
 for i = 1:numel(self)
+   queue = self(i).queue;
    self(i).queue = {};
+
+   % Evaluate eagerly without history
+   self(i).running_ = true;
 
    if self(i).lazyLoad
       self(i).times = self(i).times_;
@@ -30,5 +40,22 @@ for i = 1:numel(self)
       
       self(i).cumulOffset = 0;
       self(i).offset = self(i).offset_;
-   end   
+   end
+   
+   % Turn deferred execution back on
+   if self(i).lazyEval
+      self(i).running_ = false;
+   end
+   
+   if n
+      queue = queue(1:n,:);
+      for j = 1:size(queue,1)
+         queue{j,3} = false;
+      end
+      self(i).queue = queue;
+      
+      if ~self(i).lazyEval
+         evalOnDemand(self);
+      end
+   end
 end
