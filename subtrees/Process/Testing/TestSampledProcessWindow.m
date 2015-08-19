4012fb21ffefd8dd0d58fa868a5a84d42d175dd8
Brian Lau <brian.lau@upmc.fr>
2015-08-19 17:14:45 +0200

    ENH: testing for setWindow() and setInclusiveWindow()

--- subtrees/Process/Testing/TestSampledProcessWindow.m
+++ subtrees/Process/Testing/TestSampledProcessWindow.m
@@ -231,10 +231,73 @@ classdef TestSampledProcessWindow < matlab.unittest.TestCase
          testCase.assertTrue(all(s.isValidWindow));
       end
       
+      function setWindowWithObjectArray(testCase,Fs)
+         values1 = (0:2*ceil(Fs))'; % times range from 0 to at least 2 seconds
+         values2 = 10 + (0:2*ceil(Fs))'; % times range from 0 to at least 2 seconds
+         win1 = [0 1.5];
+         win2 = [0.5 2.5];
+         dt = 1/Fs;
+         
+         s(1) = SampledProcess('values',values1,'Fs',Fs);
+         s(2) = SampledProcess('values',values2,'Fs',Fs);
+         setWindow(s,{win1 ; win2});
+
+         testCase.assertEqual(s(1).window,win1);
+         testCase.assertEqual(s(2).window,win2);
+
+         % First SampledProcess
+         times = SampledProcess.tvec(0,dt,size(values1,1));
+         ind = (times>=win1(1)) & (times<=win1(2));
+         
+         % Expected NaN-padding
+         [pre,preV] = SampledProcess.extendPre(s(1).tStart,win1(1),dt,1);
+         [post,postV] = SampledProcess.extendPost(s(1).tEnd,win1(2),dt,1);
+         
+         testCase.assertEqual(numel(s(1).times),1);
+         testCase.assertEqual(s(1).times{1},[pre ; times(ind) ; post],'AbsTol',eps);
+         testCase.assertEqual(numel(s(1).values),1);
+         testCase.assertEqual(s(1).values{1},[preV ; values1(ind) ; postV]);
+         
+         % Second SampledProcess
+         times = SampledProcess.tvec(0,dt,size(values2,1));
+         ind = (times>=win2(1)) & (times<=win2(2));
+         
+         % Expected NaN-padding
+         [pre,preV] = SampledProcess.extendPre(s(2).tStart,win2(1),dt,1);
+         [post,postV] = SampledProcess.extendPost(s(2).tEnd,win2(2),dt,1);
+         
+         testCase.assertEqual(numel(s(2).times),1);
+         testCase.assertEqual(s(2).times{1},[pre ; times(ind) ; post],'AbsTol',eps);
+         testCase.assertEqual(numel(s(2).values),1);
+         testCase.assertEqual(s(2).values{1},[preV ; values2(ind) ; postV]);
+
+         testCase.assertEqual([s.isValidWindow],[true false]);
+      end
       
-      %testSetWindowWithObjectArray
-      
-      % setInclusiveWindow
+      function setInclusiveWindow(testCase,Fs)
+         values = (0:2*ceil(Fs))'; % times range from 0 to at least 2 seconds
+         win = [1 1.5];
+         dt = 1/Fs;
+
+         s = SampledProcess('values',values,'Fs',Fs);
+         s.window = win;
+         
+         s.setInclusiveWindow();
+         
+         times = SampledProcess.tvec(0,dt,size(values,1));
+         ind = (times>=s.window(1)) & (times<=s.window(2));
+
+         % Times outside the previous window are expected to map to NaN
+         ind2 = (times<win(1)) | (times>win(2));
+         values(ind2) = NaN;
+         values = values(ind);
+
+         testCase.assertEqual(numel(s.times),1);
+         testCase.assertEqual(s.times{1},times(ind),'AbsTol',eps);
+         testCase.assertEqual(numel(s.values),1);
+         testCase.assertEqual(s.values{1},values(ind));
+         testCase.assertTrue(s.isValidWindow);
+      end
    end
    
 end
