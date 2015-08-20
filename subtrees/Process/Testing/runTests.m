51e121a16c2fa3d0e28de8337b9d42a00876a729
Brian Lau <brian.lau@upmc.fr>
2015-08-20 12:35:04 +0200

    wrapper for running tests

---
+++ subtrees/Process/Testing/runTests.m
@@ -0,0 +1,20 @@
+% import matlab.unittest.TestSuite;
+% import matlab.unittest.TestRunner;
+% import matlab.unittest.plugins.CodeCoveragePlugin
+% 
+% suite = TestSuite.fromPwd;
+% 
+% runner = TestRunner.withTextOutput;
+% 
+% runner.addPlugin(CodeCoveragePlugin.forFolder(pwd))
+% result = runner.run(suite);
+% 
+
+suite = matlab.unittest.TestSuite.fromFolder(pwd);
+runner = matlab.unittest.TestRunner.withTextOutput;
+dir = '/Users/brian/Documents/Code/Repos/LabTools/subtrees/Process/@SampledProcess';
+runner.addPlugin(matlab.unittest.plugins.CodeCoveragePlugin.forFolder(dir));
+dir = '/Users/brian/Documents/Code/Repos/LabTools/subtrees/Process/';
+runner.addPlugin(matlab.unittest.plugins.CodeCoveragePlugin.forFolder(dir));
+result = runner.run(suite);
+table(result)
