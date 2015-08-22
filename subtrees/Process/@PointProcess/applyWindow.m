3c44bf78895272afb569d305fac09008d7c918a2
Brian Lau <brian.lau@upmc.fr>
2015-08-22 15:53:26 +0200

    BF: Fix incorrect indexing for multiple point processes

--- subtrees/Process/@PointProcess/applyWindow.m
+++ subtrees/Process/@PointProcess/applyWindow.m
@@ -10,24 +10,31 @@
 
 function applyWindow(self)
 
-nWindow = size(self.window,1);
-
-times = self.times;
-if isempty(times) % FIXME, is this only for constructor?
+if isempty(self.times) % FIXME, is this only for constructor?
    return;
 end
 
-nTimes = size(times,2);
-window = self.window;
-values = self.values;
+nTimes = size(self.times,2);
+nWindowReq = size(self.window,1);
+nWindowOrig = size(self.times,1);
+if nWindowOrig > 1
+   assert(nWindowReq==nWindowOrig,'monkey!');
+end
 
-windowedTimes = cell(nWindow,nTimes);
-windowedValues = cell(nWindow,nTimes);
-for i = 1:nWindow
+window = self.window;
+windowedTimes = cell(nWindowReq,nTimes);
+windowedValues = cell(nWindowReq,nTimes);
+for i = 1:nWindowReq
+   if nWindowOrig == 1
+      idx = 1;
+   else
+      idx = i;
+   end
+   
    for j = 1:nTimes
-      ind = (times{j}(:,1)>=window(i,1)) & (times{j}(:,1)<=window(i,2));
-      windowedTimes{i,j} = times{j}(ind,:);
-      windowedValues{i,j} = values{j}(ind);
+      ind = (self.times{idx,j}(:,1)>=window(i,1)) & (self.times{idx,j}(:,1)<=window(i,2));
+      windowedTimes{i,j} = self.times{idx,j}(ind,:);
+      windowedValues{i,j} = self.values{idx,j}(ind);
    end
 end
 self.times = windowedTimes;
