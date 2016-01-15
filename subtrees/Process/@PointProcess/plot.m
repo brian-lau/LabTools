b975ea7a4ed793c66c6276aad5f34be6949af753
Brian Lau <brian.lau@upmc.fr>
2016-01-15 14:02:58 +0100

    ENH: prevent automatic output

--- subtrees/Process/@PointProcess/plot.m
+++ subtrees/Process/@PointProcess/plot.m
@@ -1,4 +1,10 @@
-function [h,yOffset] = plot(self,varargin)
+function varargout = plot(self,varargin)
 
 [h,yOffset] = raster(self,varargin{:});
 
+if nargout == 1
+   varargout{1} = h;
+elseif nargout == 2
+   varargout{1} = h;
+   varargout{2} = yOffset;
+end
