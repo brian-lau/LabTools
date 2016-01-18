83d970518d6db136ba1bea5ab67ef6d7fbcbee2e
Brian Lau <brian.lau@upmc.fr>
2016-01-18 15:18:53 +0100

    BF: Incomplete error

--- subtrees/Process/@SampledProcess/tvec.m
+++ subtrees/Process/@SampledProcess/tvec.m
@@ -5,8 +5,8 @@
 
 function t = tvec(t0,dt,n)
 
-assert(isscalar(t0),'SampledProcess:tvec:InputFormat');
-assert(isscalar(dt),'SampledProcess:tvec:InputFormat');
-assert(isscalar(n),'SampledProcess:tvec:InputFormat');
+assert(isscalar(t0),'SampledProcess:tvec:InputFormat','1st input must be scalar');
+assert(isscalar(dt),'SampledProcess:tvec:InputFormat','2nd input must be scalar');
+assert(isscalar(n),'SampledProcess:tvec:InputFormat','3rd input must be scalar');
 
 t = t0 + (0:dt:(dt*(n-1)))';
