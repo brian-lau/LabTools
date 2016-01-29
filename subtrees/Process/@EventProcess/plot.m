9803519d93b473fdfe37464ae0778b1631353c07
Brian Lau <brian.lau@upmc.fr>
2016-01-29 10:20:14 +0100

    BF: find events within callbacks using handle object, otherwise, events (value objects) can become stale

--- subtrees/Process/@EventProcess/plot.m
+++ subtrees/Process/@EventProcess/plot.m
@@ -13,6 +13,7 @@ function varargout = plot(self,varargin)
       h = p.Results.handle;
    end
    hold on;
+   %xlim = get(h,'xlim');
    ylim = get(h,'ylim');
 
    menu = uicontextmenu();
@@ -28,21 +29,23 @@ function varargout = plot(self,varargin)
       right = values(i).time(2);
       bottom = ylim(1);
       top = ylim(2);
-      eFill(i) = fill([left left right right],[bottom top top bottom],...
-         c(i,:),'FaceAlpha',0.15,'EdgeColor','none','Parent',h);
-      set(eFill(i),'Tag',values(i).name);
-
+      
       eventMenu = uicontextmenu();
-      m1 = uimenu('Parent',eventMenu,'Label','Edit','Callback',{@editEvent values(i)});
-      m2 = uimenu('Parent',eventMenu,'Label','Move','Callback',{@moveEvent eFill(i) self i h});
-      m3 = uimenu('Parent',eventMenu,'Label','Delete','Callback',{@deleteEvent self values(i) h});
+      m1 = uimenu('Parent',eventMenu,'Label','Edit','Callback',{@editEvent self});
+      m2 = uimenu('Parent',eventMenu,'Label','Move','Callback',{@moveEvent self h});
+      m3 = uimenu('Parent',eventMenu,'Label','Delete','Callback',{@deleteEvent self h});
       set([m1 m2 m3],'Tag',values(i).name);
-      set(eFill(i),'uicontextmenu',eventMenu);
+      
+      eFill = fill([left left right right],[bottom top top bottom],...
+         c(i,:),'FaceAlpha',0.15,'EdgeColor','none','Parent',h);
+      set(eFill,'Tag',values(i).name);
+      set(eFill,'uicontextmenu',eventMenu);
 
-      eText(i) = text(left,top,values(i).name,'VerticalAlignment','bottom',...
+      eText = text(left,top,values(i).name,'VerticalAlignment','bottom',...
          'FontAngle','italic','Parent',h);
-      set(eText(i),'Tag',values(i).name);
+      set(eText,'Tag',values(i).name);
    end
+   %set(h,'xlim',xlim);
 
    if nargout >= 1
       varargout{1} = h;
@@ -50,31 +53,40 @@ function varargout = plot(self,varargin)
 end
 
 function add(source,data,obj,h)
-d = dragRect('xx');
-set(d,'EndDragCallback',@(hobj,evnt)disp('now drag end'));
-keyboard
+   d = dragRect('xx');
+   set(d,'EndDragCallback',@(hobj,evnt)disp('now drag end'));
+   keyboard
 end
 
-function editEvent(~,~,event)
-   propertiesGUI(event)
+function editEvent(source,~,obj)
+	event = find(obj,'name',source.Tag);
+   propertiesGUI(event);
 end
 
-function moveEvent(source,~,p,obj,ind,h)
+function moveEvent(source,~,obj,h)
+   p = findobj(h,'Tag',source.Tag,'-and','Type','patch');
+   setptr(gcf,'hand');
    fig.movepatch(p,'x');
    waitfor(gcf,'UserData','stop');
    set(gcf,'UserData',[]);
-   
    v = get(p,'Vertices');
-   temp = obj.values{1}(ind);
-   temp.tStart = v(1,1);
-   temp.tEnd = v(3,1);
-   obj.values{1}(ind) = temp;
+   
+   q = linq(obj.values{1});
+   ind = find(q.select(@(x) strcmp(x.name,source.Tag)).toArray());
+   event = obj.values{1}(ind);
+
+   event.tStart = v(1,1);
+   event.tEnd = v(3,1);
+   obj.values{1}(ind) = event;
    obj.times{1}(ind,:) = [v(1,1) v(3,1)];
    g = findobj(h,'Tag',source.Tag,'-and','Type','Text');
    g.Position(1) = v(1,1);
 end
 
-function deleteEvent(source,~,obj,event,h)
+function deleteEvent(source,~,obj,h)
+   % could use    get(gcf, 'CurrentObject') or perhaps hittest to resolve
+   % multiple events (eg, same name)
+	event = find(obj,'name',source.Tag);
    obj.remove(event.time(1));
    g = findobj(h,'Tag',source.Tag);
    delete(g);
