5d45fcd91bd1794020d9f772f0cf968e5bb7d0c6
Brian Lau <brian.lau@upmc.fr>
2016-02-10 13:32:32 +0100

    BF: selection_ correctly updated in fix()

--- subtrees/Process/@Process/fix.m
+++ subtrees/Process/@Process/fix.m
@@ -1,8 +1,11 @@
+% Make permanent current data transformations
+
 function self = fix(self)
 
 for i = 1:numel(self)
    self(i).Fs_ = self(i).Fs;
    self(i).quality_ = self(i).quality;
+   self(i).selection_ = self(i).selection_(self(i).selection_);
    self(i).labels_ = self(i).labels;
    self(i).times_ = self.times;
    self(i).values_ = self(i).values;
