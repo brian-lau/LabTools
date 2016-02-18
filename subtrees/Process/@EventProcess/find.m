cb2da774b656613626857444eeb8cfe0cc70f1dd
Brian Lau <brian.lau@upmc.fr>
2016-02-18 13:11:31 +0100

    Reworked EventProcess.find, needs some optimisation

--- subtrees/Process/@EventProcess/find.m
+++ subtrees/Process/@EventProcess/find.m
@@ -1,23 +1,30 @@
 % object array
 % multiple windows
 % arbitrary function_handle
+% output policy (restrict to one event?)
+% multiple event labels
 function ev = find(self,varargin)
 
 p = inputParser;
-p.KeepUnmatched= true;
 p.FunctionName = 'EventProcess find';
 p.addParameter('eventType',[],@ischar);
 p.addParameter('eventProp','name',@(x) ischar(x) || iscell(x));
 p.addParameter('eventVal',[]);
+p.addParameter('func',[],@(x) isa(x,'function_handle'));
 p.addParameter('logic','or',@(x) any(strcmp(x,{'or' 'union' 'and' 'intersection' 'xor' 'setxor'})));
 p.parse(varargin{:});
 par = p.Results;
 
-args = p.Unmatched;
+nObj = numel(self);
+for i = 1:nObj
+   ev(i) = findEach(self(i),par);
+end
 
-events = self.values{1};
+function ev = findEach(obj,par)
+
+events = obj.values{1};
 if isempty(events)
-   ev = self.nullEvent;
+   ev = obj.nullEvent;
    return;
 end
 
@@ -26,6 +33,7 @@ if ~isempty(par.eventType) % requires full event match (ignores eventProp/Val)
    eventTypeInd = strcmpi(arrayfun(@(x) class(x),events,'uni',0),par.eventType);
 end
 
+eventPropInd = [];
 if ~isempty(par.eventVal)
    % labels are heterogenous, so we filter out elements that do not possess
    % the property of interest (treated as false)
@@ -51,60 +59,53 @@ if ~isempty(par.eventVal)
    end
 
    % Reinsert the matching indices for the reduced subset back into full index
-   eventPropInd = false(1,self.n);
+   eventPropInd = false(1,obj.n);
    eventPropInd(matchProp(match(I))) = true;
    eventPropInd = find(eventPropInd);
 end
-   
-% switch lower(par.logic)
-%    case {'or' 'union'}
-      selection = unionm(eventTypeInd,eventPropInd);
-%    case {'and' 'intersection'}
-%       if ~isempty(par.index)
-%          selection = indexInd;
-%       else
-%          selection = baseInd;
-%       end
-%       if ~isempty(par.label)
-%          selection = intersect(selection,labelInd);
-%       end
-%       if ~isempty(par.quality)
-%          selection = intersect(selection,qualityInd);
-%       end
-%    case {'xor' 'setxor'}
-%       selection = setxor(baseInd,unionm(indexInd,labelInd,qualityInd));
-% end
+
+funcInd = [];
+if ~isempty(par.func)
+   % TODO cell array of function handles, allow multiple arbitrary crit
+   funcInd = find(arrayfun(par.func,events,'ErrorHandler',@errorhandler));
+end
+
+baseInd = 1:numel(events);
+selection = [];
+switch lower(par.logic)
+   case {'or' 'union'}
+      selection = unique(vertcat(eventTypeInd(:),eventPropInd(:),funcInd(:)));
+      %selection = unionm(eventTypeInd,eventPropInd,funcInd);
+   case {'and' 'intersection'}
+      if ~isempty(eventTypeInd)
+         selection = eventTypeInd;
+      else
+         selection = baseInd;
+      end
+      if ~isempty(eventPropInd)
+         selection = intersect(selection,eventPropInd);
+      end
+      if ~isempty(funcInd)
+         selection = intersect(selection,funcInd);
+      end
+   case {'xor' 'setxor'}
+      selection = setxor(baseInd,unionm(eventTypeInd,eventPropInd,funcInd));
+end
 
 if isempty(selection)
-   ev = self.nullEvent;
+   ev = obj.nullEvent;
 else
    ev = events(selection);
 end
 
-% keyboard
-% query = linq(self.values{1});
-% fn = fieldnames(args);
-% for i = 1:numel(fn)
-%    if query.count>0
-%       if isa(args.(fn{i}),'function_handle')
-%          % This must evaluate to a boolean
-%          query.where(args.(fn{i}));
-%       elseif ischar(args.(fn{i}))
-%          try
-%             query.where(@(x) strcmp(x.(fn{i}),args.(fn{i})));
-%          catch
-%          end
-%          %                   query.where(@(x) isprop(x,fn{i}))...
-%          %                        .where(@(x) strcmp(x.(fn{i}),args.(fn{i})));
-%       else
-%          % attempt equality
-%          query.where(@(x) x.(fn{i})==args.(fn{i}));
-%       end
-%    end
-% end
-% 
-% if query.count > 0
-%    ev = query.toArray();
-% else
-%    ev = self.nullEvent;
-% end
+%%
+function result = errorhandler(err,varargin)
+if strcmp(err.identifier,'MATLAB:noSuchMethodOrField');
+   result = false;
+else
+   err = MException(err.identifier,err.message);
+   cause = MException('EventProcess:find:func',...
+      'Problem in function handle.');
+   err = addCause(err,cause);
+   throw(err);
+end
