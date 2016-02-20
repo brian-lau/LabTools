11123b004b5579354ee83f038372d0273c1807cf
Brian Lau <brian.lau@upmc.fr>
2016-02-20 17:46:41 +0100

    ENH: Give metadata.event.Generic a property

--- subtrees/matmeta/+metadata/+event/Generic.m
+++ subtrees/matmeta/+metadata/+event/Generic.m
@@ -1,7 +1,22 @@
 classdef Generic < metadata.Event
+   properties
+      value
+   end
    methods
       function self = Generic(varargin)
          self = self@metadata.Event(varargin{:});
+         if nargin == 0
+            return;
+         end
+         
+         p = inputParser;
+         p.KeepUnmatched= true;
+         p.FunctionName = 'Generic constructor';
+         p.addParameter('value',[]);
+         p.parse(varargin{:});
+         par = p.Results;
+         
+         self.value = par.value;
       end
    end
 end
