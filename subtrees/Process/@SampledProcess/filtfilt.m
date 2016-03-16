fdad6c8bb4d9f0d7421c912be14a91c09e7a9b19
Brian Lau <brian.lau@upmc.fr>
2016-03-16 09:45:34 +0100

    DOC: SampledProcess.filtfilt() documentation clarified to make more explicit the difference from SampledProcess.filter() and when it should(not) be used

--- subtrees/Process/@SampledProcess/filtfilt.m
+++ subtrees/Process/@SampledProcess/filtfilt.m
@@ -9,12 +9,18 @@
 %     taken into account when designing filters or determining the actual 
 %     attenuation and cutoffs applied to filtered data.
 %
+%     Do not use filtfilt if you are filtering with linear-phase FIR
+%     filters. Besides the issue above of changing ripple/attenuation, it
+%     is much slower than SampledProcess.filter (see example). filtfilt is 
+%     useful when using IIR/nonlinear-phase FIR filters and zero-phase
+%     shift is required.
+%
 % INPUTS
 %     f - numeric vector | dfilt object | designFilter object, required
 %
 % EXAMPLES
 %     t = (0:.001:1)';
-%     s = SampledProcess(cos(2*pi*10*t)+0.25*randn(length(t),1),'Fs',1000);
+%     s = SampledProcess(cos(2*pi*7*t)+0.25*randn(length(t),1),'Fs',1000);
 %     % Linear-phase lowpass FIR filter with passband ending at 20 Hz
 %     d = fdesign.lowpass('Fp,Fst,Ap,Ast',20,40,0.01,60,s.Fs);
 %     f = design(d,'equiripple','minOrder','even');
@@ -24,7 +30,20 @@
 %
 %     % compare to causal filter
 %     s.reset().filter(f,'compensateDelay',false).plot('handle',h);
-%     legend('original','filtfilt','filter - no compensation');
+%
+%     % In the case of linear-phase FIR filters, there is no reason to use
+%     % filtfilt, which is much slower than filter (one-pass, FFT)
+%     s.reset;
+%     [~,f,d,hh] = s.lowpass('Fpass',20,'Fstop',40,'designOnly',true,...
+%        'plot',0,'verbose',0);
+%     % Two-pass filtfilt
+%     h = plot(s.filtfilt(f));
+%     % One-pass filter. Filters are linear, and the cascade of two filters
+%     % is equivalent to filtering with the convolution of the two filters
+%     s.reset;
+%     plot(s.filter(conv(f.Numerator,f.Numerator)),'handle',h)
+%     % Differences occur at the edges, due to different treatment of end
+%     % effects
 %
 %     SEE ALSO
 %     filter
