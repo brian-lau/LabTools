1bb51068b8d33fd11e62e9fd56f94119d597d4c9
Brian Lau <brian.lau@upmc.fr>
2016-03-28 09:10:52 +0200

    initial commit stat.arspectrum

---
+++ subtrees/matutils/+stat/arspectrum.m
@@ -0,0 +1,19 @@
+% Power spectral density of univariate autoregressive process
+% A     - coefficients
+% sigma - noise standard deviation
+% Fs    - sampling frequency
+% f     - frequencies at which to evaluate spectrum
+function psd = arspectrum(A,sigma,Fs,f)
+
+if nargin < 4
+   f = (0:(Fs/2))';
+else
+   f = f(:);
+end
+
+psd = zeros(size(f));
+for i = 1:numel(A)
+   psd = psd + A(i)*exp(-1j*2*pi*i*f/Fs);
+end
+psd = abs(1-psd).^2;
+psd = sigma^2/Fs./psd;
