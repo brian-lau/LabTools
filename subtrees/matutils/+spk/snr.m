0a6b698c7d5e08dde435b3bf8ea8e8347e1296c7
Brian Lau <brian.lau@upmc.fr>
2016-06-07 08:26:49 +0200

    DOC: references

--- subtrees/matutils/+spk/snr.m
+++ subtrees/matutils/+spk/snr.m
@@ -1,4 +1,11 @@
 % waveforms [nT x nSpikes]
+% Joshua M, Elias S, Levine O, Bergman H. Quantifying the isolation quality 
+% of extracellularly recorded action potentials.
+% J. Neurosci. Methods 2007; 163: 267?82.
+
+% Stratton P, Cheung A, Wiles J, Kiyatkin E, Sah P, Windels F. Action potential 
+% waveform variability limits multi-unit separation in freely behaving rats. 
+% PLoS One 2012; 7: e38482.
 function results = snr(waveforms,varargin)
 p = inputParser;
 p.KeepUnmatched = false;
@@ -12,6 +19,7 @@ nT = size(waveforms,1);
 
 if ~isempty(par.labels)
    uLabels = unique(par.labels);
+   results(numel(uLabels)) = struct('p2p',[],'rms1',[],'rms2',[]);
    for i = 1:numel(uLabels)
       ind = par.labels == uLabels(i);
       results(i) = spk.snr(waveforms(:,ind),'noise',par.noise);
@@ -23,17 +31,18 @@ else
    %%
    p2p = max(meanwf) - min(meanwf);
    if isempty(par.noise)
+      % Estimate noise as residuals
       noise = bsxfun(@minus,waveforms,meanwf);
    else
       noise = par.noise;
    end
    
-   % From Bergman paper
+   % Eq 7 Joshua et al.
    results.p2p =  p2p / (5*std(noise(:)));
    
    % from U. Rutishauser, E.M. Schuman, A.N. Mamelak 2006
    results.rms1 = norm(meanwf) /  sqrt(nT*var(noise(:)));
    
-   % http://www.plosone.org/article/info:doi/10.1371/journal.pone.0038482
+   % pg 3 Stratton et al.
    results.rms2 = rms(waveforms(:)) / rms(noise(:));
 end
