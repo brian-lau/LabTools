b832f6100658758257faf9c5e7e09638d7a2e2da
Brian Lau <brian.lau@upmc.fr>
2017-02-03 18:04:09 +0100

    testing deal speed

--- subtrees/Process/@EventProcess/updateEventTimes.m
+++ subtrees/Process/@EventProcess/updateEventTimes.m
@@ -1,14 +1,24 @@
 % Adjust times stored in Events
 function updateEventTimes(self,varargin)
 
-for i = 1:size(self.times,1) % channels
-   for j = 1:size(self.times,2) % windows
-      temp = self.values{i,j};
-      times = self.times{i,j};
-      for k = 1:numel(temp)
-         temp(k).tStart = times(k,1);
-         temp(k).tEnd = times(k,2);
+if 1
+   for i = 1:size(self.times,1) % channels
+      for j = 1:size(self.times,2) % windows
+         temp = num2cell(self.times{i,j});
+         [self.values{i,j}.tStart] = deal(temp{:,1});
+         [self.values{i,j}.tEnd] = deal(temp{:,2});
       end
-      self.values{i,j} = temp;
+   end
+else
+   for i = 1:size(self.times,1) % channels
+      for j = 1:size(self.times,2) % windows
+         temp = self.values{i,j};
+         times = self.times{i,j};
+         for k = 1:numel(temp)
+            temp(k).tStart = times(k,1);
+            temp(k).tEnd = times(k,2);
+         end
+         self.values{i,j} = temp;
+      end
    end
 end
