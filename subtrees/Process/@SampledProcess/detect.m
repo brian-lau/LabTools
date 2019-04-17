7a2cd09f36eb4c9647e2c54ccf799e6bfc35498d
Brian Lau <brian.lau@upmc.fr>
2019-04-17 17:33:59 +0200

    Start working on detection for bursts

--- subtrees/Process/@SampledProcess/detect.m
+++ subtrees/Process/@SampledProcess/detect.m
@@ -8,29 +8,46 @@ else
    % TODO : implement standard detectors
 end
 s.map(func);
+keyboard
 
-ind = s.values{1};
-times = s.times{1};
-
-dind = diff(ind); % +1 event goes hi, -1 event goes lo
-ascend = times(dind==1);
-descend = times(dind==-1);
-
-if isempty(ascend) || isempty(descend)
-   events = [NaN NaN];
-else
-   if descend(1) < ascend(1)
-      descend(1) = [];
-   end
+for i = 1:numel(s)
+   ind = s(i).values{1};
+   times = s(i).times{1};
+   %times = repmat(times,s(i).n);
    
-   if ascend(end) > descend(end)
-      ascend(end) = [];
-   end
+   dind = diff(ind); % +1 event goes hi, -1 event goes lo
    
-   events = [ascend , descend];
+   for j = 1:s(i).n
+      ascend = times(dind(:,j)==1);
+      descend = times(dind(:,j)==-1);
+      if isempty(ascend) || isempty(descend)
+         %events{i,j} = [NaN NaN];
+         events{i,j} = [];
+      else
+         if descend(1) < ascend(1)
+            descend(1) = [];
+         end
+         
+         if ascend(end) > descend(end)
+            ascend(end) = [];
+         end
+         
+         events{i,j} = [ascend , descend];
+      end
+      
+      n = size(events{i,j},1);
+      ev(n) = metadata.event.Generic();
+      
+      tStart = num2cell(events{i,j}(:,1));
+      [ev.tStart] = deal(tStart{:});
+      tEnd = num2cell(events{i,j}(:,2));
+      [ev.tEnd] = deal(tEnd{:});
+      
+      evv{j} = ev;
+      clear ev;
+   end
+   %ep(i) = EventProcess(evv,'tStart',self(i).tStart,'tEnd',self(i).tEnd);
 end
-
-ev = events;
 
 % n = size(events,1);
 % ev(n) = metadata.event.Generic();
@@ -40,4 +57,6 @@ ev = events;
 % tEnd = num2cell(events(:,2));
 % [ev.tEnd] = deal(tEnd{:});
 % 
-%ep = EventProcess(ev,'tStart',self.tStart,'tEnd',self.tEnd);
+% ep = EventProcess(ev,'tStart',self.tStart,'tEnd',self.tEnd);
+% 
+% for 
